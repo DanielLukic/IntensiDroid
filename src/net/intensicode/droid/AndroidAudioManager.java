ac71cbc0c8ceab67e8698c0a5ced442f319648be
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-06 00:37:44 +0100

    Removed unused OnErrorListener implementation from AndroidAudioManager.

--- src/net/intensicode/droid/AndroidAudioManager.java
+++ src/net/intensicode/droid/AndroidAudioManager.java
@@ -1,15 +1,11 @@
 package net.intensicode.droid;
 
 import android.content.Context;
-import android.media.MediaPlayer;
 import net.intensicode.core.*;
 import net.intensicode.droid.audio.*;
-import net.intensicode.util.Log;
 
-public final class AndroidAudioManager extends AudioManager implements MediaPlayer.OnErrorListener
+public final class AndroidAudioManager extends AudioManager
     {
-    private static final int UNKNOWN_NUMBER_OF_CHANNELS = -1;
-
     public AndroidAudioManager( final Context aContext )
         {
         mySoundBackend = new SoundPoolBackend( aContext.getAssets() );
@@ -26,16 +22,6 @@ public final class AndroidAudioManager extends AudioManager implements MediaPlay
     protected AudioResourceEx loadSoundUnsafe( final String aSoundName ) throws Exception
         {
         return mySoundBackend.loadSound( aSoundName );
-        }
-
-    // From OnErrorListener
-
-    public boolean onError( final MediaPlayer aMediaPlayer, final int i, final int i1 )
-        {
-        //#if DEBUG
-        Log.debug( "media player error {} {}", i, i1 );
-        //#endif
-        return false;
         }
 
 
