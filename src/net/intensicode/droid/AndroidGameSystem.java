3fb0e3a60b3638299bba99b0f6f0575b613c6f10
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-24 20:57:55 +0100

    Implemented AndroidGameSystem#fillInformationStrings with info from Build SDK class and info from OpenglGameView and OpenglGraphics.

--- src/net/intensicode/droid/AndroidGameSystem.java
+++ src/net/intensicode/droid/AndroidGameSystem.java
@@ -1,6 +1,9 @@
 package net.intensicode.droid;
 
+import android.os.Build;
 import net.intensicode.core.*;
+import net.intensicode.droid.opengl.*;
+import net.intensicode.util.*;
 
 public final class AndroidGameSystem extends GameSystem
     {
@@ -14,5 +17,34 @@ public final class AndroidGameSystem extends GameSystem
     protected void throwWrappedExceptionToTellCallingSystemAboutBrokenGameSystem( final Exception aException )
         {
         throw new RuntimeException( "failed showing error screen", aException );
+        }
+
+    protected final void fillInformationStrings()
+        {
+        myInformationStrings.add( Build.BRAND );
+        myInformationStrings.add( Build.MODEL );
+        myInformationStrings.add( Build.DEVICE );
+        myInformationStrings.add( Build.DISPLAY );
+        myInformationStrings.add( Build.PRODUCT );
+
+        if ( screen instanceof OpenglGameView )
+            {
+            final OpenglGameView view = (OpenglGameView) screen;
+            myInformationStrings.add( view.getArgbString() );
+            }
+
+        if ( graphics instanceof OpenglGraphics )
+            {
+            final OpenglGraphics opengl = (OpenglGraphics) graphics;
+            myInformationStrings.add( opengl.vendor );
+            myInformationStrings.add( opengl.renderer );
+            myInformationStrings.add( opengl.version );
+
+            final DynamicArray extensions = StringUtils.splitString( opengl.extensions, " ", true );
+            for ( int idx = 0; idx < extensions.size; idx++ )
+                {
+                myInformationStrings.add( extensions.get( idx ) );
+                }
+            }
         }
     }
