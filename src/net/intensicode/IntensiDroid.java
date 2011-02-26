4d4c34948680469e453c617fd7203fd66b770fdd
Daniel Lukic <daniel.lukic@berlinfactor.com>
2011-02-26 11:05:33 +0100

    Added support for inneractive advertising.

--- src/net/intensicode/IntensiDroid.java
+++ src/net/intensicode/IntensiDroid.java
@@ -703,6 +703,32 @@ public abstract class IntensiDroid extends DebugLifeCycleActivity implements Pla
         }
         //#endif
 
+        //#if IA
+        {
+        myGameView.setId( 0x1723CAFE );
+        myGameView.setFocusable( true );
+        myGameView.requestFocus();
+        myGameView.requestFocusFromTouch();
+
+        final com.innerActive.ads.InnerActiveAdView adView = new com.innerActive.ads.InnerActiveAdView( this );
+        adView.setLayoutParams( new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT ) );
+        adView.setId( 0x1723BABE );
+        adView.setFocusable( false );
+        adView.setBgColor( 0x000000 );
+        adView.setTxtColor( 0xFFFFFFFF );
+        adView.refreshAd();
+        adView.setRefreshInterval( 60 );
+        adView.setEnabled( true );
+        myBannerAd = adView;
+
+        final RelativeLayout layout = new RelativeLayout( this );
+        layout.addView( myGameView );
+        layout.addView( adView );
+
+        setContentView( layout );
+        }
+        //#endif
+
         //#if MOBCLIX
         {
         com.mobclix.android.sdk.Mobclix.onCreate( this );
@@ -780,7 +806,7 @@ public abstract class IntensiDroid extends DebugLifeCycleActivity implements Pla
         }
         //#endif
 
-        //#if !ADMOB && !GREY && !MOBCLIX
+        //#if !ADMOB && !GREY && !MOBCLIX && !IA
         //# myGameView.setId( 0x1723CAFE );
         //# setContentView( myGameView );
         //#endif
